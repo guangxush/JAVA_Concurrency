01dbe5ce38c42b847b0364a90e8a62f7a6ea8e80
gshan <guangxush@163.com>
2018-09-09 23:19:56 +0800

    gitignore

---
+++ src/chapter/three/FinalExample.java
@@ -0,0 +1,24 @@
+package chapter.three;
+
+/**
+ * Created by gshan on 2018/9/9
+ */
+public class FinalExample {
+
+    int i;//普通变量
+    final int j;//final变量
+    static FinalExample obj;
+    public FinalExample(){//构造函数
+        i=1;//普通变量
+        j=2;//写final变量
+        //storestore屏障
+    }
+    public static void writer(){//写线程A执行
+        obj = new FinalExample();
+    }
+    public static void reader(){//读线程B执行
+        FinalExample object = obj;//读对象引用
+        int a = object.i;//读普通域
+        int b = object.j;//读final域
+    }
+}
