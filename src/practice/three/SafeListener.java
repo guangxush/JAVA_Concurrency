5616bec0be9f446330f0d2908c4528c08a50ad0b
guangxush <guangxush@163.com>
2018-10-13 23:04:43 +0800

    safe new instances

---
+++ src/practice/three/SafeListener.java
@@ -0,0 +1,22 @@
+package practice.three;
+
+import javafx.event.Event;
+
+import java.util.EventListener;
+
+public class SafeListener {
+    private final EventListener listener;
+
+    private SafeListener(){
+        listener = new EventListener() {
+            public void onEvnet(Event event){
+                dosomething(e);
+            }
+        }
+    }
+    public static SafeListener newInstance(EventListener source){
+        SafeListener safe = new SafeListener();
+        source.registerListener(safe.listener);
+        return safe;
+    }
+}
