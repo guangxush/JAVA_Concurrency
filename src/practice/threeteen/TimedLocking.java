b316b36d0212066cd105e8e79002889aa19ccb4d
guangxush <guangxush@163.com>
2018-10-26 23:06:27 +0800

    threeteen

---
+++ src/practice/threeteen/TimedLocking.java
@@ -0,0 +1,34 @@
+package practice.threeteen;
+
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
+import static java.util.concurrent.TimeUnit.NANOSECONDS;
+
+public class TimedLocking {
+    private Lock lock = new ReentrantLock();
+
+    public boolean trySendOnSharedLine(String message,
+                                       long timeout, TimeUnit unit)
+            throws InterruptedException {
+        long nanosToLock = unit.toNanos(timeout)
+                - estimatedNanosToSend(message);
+        if (!lock.tryLock(nanosToLock, NANOSECONDS))
+            return false;
+        try {
+            return sendOnSharedLine(message);
+        } finally {
+            lock.unlock();
+        }
+    }
+
+    private boolean sendOnSharedLine(String message) {
+        /* send something */
+        return true;
+    }
+
+    long estimatedNanosToSend(String message) {
+        return message.length();
+    }
+}
